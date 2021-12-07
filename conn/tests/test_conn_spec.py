cadf69f19079515fcb15225a2a4288ae8fc7ba42
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-12-07 08:44:53 +0100

    added template for tests

---
+++ conn/tests/test_conn_spec.py
@@ -0,0 +1,20 @@
+import numpy as np  # For np.testing
+
+
+class TestConnSpec:
+
+    def test_conn_tf(self, ):
+        pass
+
+    def test_wavelet_spec(self, ):
+        pass
+
+    def test_conn_spec_coh(self,):
+        pass
+
+    def test_conn_spec_plv(self,):
+        pass
+
+
+if __name__ == "__main__":
+    pass
